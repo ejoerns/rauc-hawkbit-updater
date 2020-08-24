82410ef91b9bf6607ca360fa862cf3990d7bb815
Enrico Joerns <ejo@pengutronix.de>
2020-08-24 02:24:54 +0200

    test/test.py: interact with an actual hawkbit instance
    Signed-off-by: Enrico Joerns <ejo@pengutronix.de>

--- test/test.py
+++ test/test.py
@@ -1,13 +1,39 @@
 import pytest
 import sys, os, subprocess
 import re
+import logging
+
+import hawkbit
+from hawkbit import HawkbitTestClient
+from hawkbit import HawkbitError
+
+from tempfile import NamedTemporaryFile
 
 here = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "/build"
 os.environ["PATH"] = here + ":" + os.environ["PATH"]
 
-@pytest.fixture
-def hawkbit_running():
-    pass
+@pytest.fixture(scope="session")
+def hawkbit_configured():
+    hawkbit = HawkbitTestClient("localhost", "8080", username="admin", password="admin")
+
+    filename_a = os.path.dirname(os.path.abspath(__file__)) + "/dummy-bundle.raucb"
+
+    # Set config parameters
+    hawkbit.set_config("pollingTime", "00:00:30")
+    hawkbit.set_config("pollingOverdueTime", "00:03:00")
+    
+    # Add expected target
+    hawkbit.add_target("test-controller", "bhVahL1Il1shie2aj2poojeChee6ahShu")
+
+    # Create modules with artifacts
+    module_id_a = hawkbit.add_swmodule('Test module')
+    hawkbit.add_artifact(module_id_a, filename_a)
+
+    # Create distributions of it
+    dist_id = hawkbit.add_distributionset(module_id_a, name="Poky-Test")
+    logging.info("Added demo distributions")
+
+    hawkbit.assign_target(dist_id, "test-controller")
 
 def run(command):
     proc = subprocess.Popen(command,
@@ -44,3 +70,27 @@ def test_rauc_hawkbit_non_existing_config_file():
     assert exitcode == 3
     assert out == b''
     assert err == b'No such configuration file: does-not-exist.conf\n'
+
+def test_rauc_hawkbit_register_and_check_valid_auth(hawkbit_configured):
+    with NamedTemporaryFile(mode="w", delete=False) as tmp_source:
+        with open("test-config.conf") as source:
+            for line in source.readlines():
+                tmp_source.write(re.sub(r'TEST_TOKEN', r'bhVahL1Il1shie2aj2poojeChee6ahShu', line))
+    command = ["rauc-hawkbit-updater", "-c", tmp_source.name, "-r"]
+    print(command)
+    out, err, exitcode = run(command)
+    assert exitcode == 0
+    assert re.match(b'MESSAGE: Checking for new software....*', out)
+    assert err == b''
+
+def test_rauc_hawkbit_register_and_check_invalid_auth(hawkbit_configured):
+    with NamedTemporaryFile(mode="w", delete=False) as tmp_source:
+        with open("test-config.conf") as source:
+            for line in source.readlines():
+                tmp_source.write(re.sub(r'TEST_TOKEN', r'ahVahL1Il1shie2aj2poojeChee6ahShu', line))
+    command = ["rauc-hawkbit-updater", "-c", tmp_source.name, "-r"]
+    print(command)
+    out, err, exitcode = run(command)
+    assert exitcode == 1
+    assert re.match(b'MESSAGE: Checking for new software....*', out)
+    assert err == b'CRITICAL: Failed to authenticate. Check if auth_token is correct?\n'
